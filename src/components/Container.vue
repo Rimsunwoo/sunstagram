b4abe6a72a120615b6d0aa59fab74e32e125bb33
Sunwoo <dlatjsdn205@Naver.com>
2022-11-02 16:39:35 +0900

    [INITIAL] vue3 project Sunstagram

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post :postData="postData[0]" />
+    <Post :postData="postData[1]" />
+    <Post :postData="postData[2]" />
+  </div>
+</template>
+
+<script>
+import Post from "../components/Post.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Post,
+  },
+  props: {
+    postData: Array,
+  },
+};
+</script>
+
+<style></style>
